c0c78e46258ed029806c9a7ed1c89ca58a3afdc0
Rohistan <baiganyagana@gmail.com>
2026-05-19 17:09:19 +0200

    Second commit refactored manu

--- menu.py
+++ menu.py
@@ -43,10 +43,7 @@ def show_welcome():
     :return:
     """
     print("=" * 50)
-    print("Welcome".center(50))
-    print("To".center(50))
-    print("W".center(50))
-    print("Pedia".center(50))
+    print("Welcome to W-Pedia".center(50))
     print("=" * 50)
 
 
@@ -63,46 +60,42 @@ def show_welcome():
 def show_menu():
     """
 
-    print("\nMain Menu")
-    print("1. Start the game")
-    print("2. Instructions")
-    print("3. Settiongs")
-    print("4. Quit")
+    :return:
+    """
 
-    choice = input("\nEnter your choice: ")
-
-    return choice
+    choice = multiple_choice_menu( "\nMain Menu", ["Start the game", "Instructions", "Settings", "Quit"])
 
 
-def start_game():
 
-    print("\nWhich 'Mode' would you like to play today?\n")
+    if choice == "Start the game":
+        game_mode, game_category, number_of_players = start_game()
+        return game_mode, game_category, number_of_players
 
+    elif choice == "Instructions":
 
-    print("1. Who")
-    print("2. What")
-    print("3. When")
-    print("4. Where")
+        print("\nInstructions:")
+        print("This manu is under construction.")
+        print("Learn while having fun and exploring new ideas!")
+        show_menu()
 
-    mode = input("\nWhich 'Mode' would you like to play today? ")
+    elif choice == "Settings":
 
-        print("\nAmazing choice!")
-        print("Welcome to WHO mode!")
-        print("Here we will dive into the lives of remarkable women and men")
-        print("whose names shaped history, science, art, leadership, and discovery.")
-        print("Prepare to uncover inspiring stories, legendary achievements,")
-        print("and the fascinating journeys behind some of the world's greatest names!")
+        print("\nSettings menu coming soon!")
+        print("Here you will later customize your game experience.")
+        show_menu()
 
-        print("\nExcellent choice!")
-        print("Welcome to WHAT mode!")
-        print("Here you will explore incredible objects, inventions, ideas,")
-        print("mysteries, and discoveries from around the world.")
-        print("Get ready to challenge your curiosity and learn amazing facts")
-        print("about things that changed humanity forever!")
+    elif choice == "Quit":
 
-    if mode == "1":
+        print("\nThank you for playing W-Pedia!")
+        print("See you again soon, explorer!\n")
+        show_menu()
 
-    elif mode == "3":
+        #TODO! make a quit_game() function or use quit_game() from game.py
+
+    else:
+
+        print("\nInvalid choice.")
+        print("Please enter 1, 2, 3, or 4.")
 
 
 
